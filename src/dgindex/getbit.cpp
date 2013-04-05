dbc2bdeb18f60f74b11e448c520d25c1b183052e
Masaki Tanaka <maki.rxrz@gmail.com>
2013-04-05 23:54:27 +0900

    DGIndex: Fix the crash by invalid access
    This is a correction for the following commit.
    Git HASH: a8f24f554937c8b5e22c149e4254e5455d434e9e

--- src/dgindex/getbit.cpp
+++ src/dgindex/getbit.cpp
@@ -521,7 +521,7 @@ retry_sync:
                        NULL, MB_OK | MB_ICONERROR);
             ThreadKill(MISC_KILL);
         }
-        else if (Start_Flag && !pmt_check && time - start > 500)
+        else if ((Start_Flag || process.locate == LOCATE_SCROLL) && !pmt_check && time - start > 500)
         {
             pat_parser.InitializePMTCheckItems();
             pmt_check = true;
@@ -1412,7 +1412,7 @@ oops2:
             if (AudioOnly_Flag && Info_Flag && !(AudioPktCount++ % 128))
                 UpdateInfo();
         }
-        else if (Start_Flag && pmt_check && num_pmt_pids)
+        else if ((Start_Flag || process.locate == LOCATE_SCROLL) && pmt_check && num_pmt_pids)
         {
             int read_size = 0;
             int parse_pmt = pat_parser.CheckPMTSection( tp.pid, Rdptr, Packet_Length, &read_size, check_num_pmt );
