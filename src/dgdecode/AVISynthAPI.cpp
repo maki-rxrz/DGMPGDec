maki-rxrz/DGMPGDec

commit 6e7200d828b5f6066e821f1e0030fdb476570aa6

DGDecode: Update avisynth sources to v2.6
v2.5 is unsupported.

src/dgdecode/AVISynthAPI.cpp
@@ -1770,8 +1770,12 @@ AVSValue __cdecl Create_Deblock(AVSValue args, void* user_data, IScriptEnvironme
         env);
 }
 
-extern "C" __declspec(dllexport) const char* __stdcall AvisynthPluginInit2(IScriptEnvironment* env)
+const AVS_Linkage *AVS_linkage = NULL;
+
+extern "C" __declspec(dllexport) const char* __stdcall
+AvisynthPluginInit3(IScriptEnvironment* env, const AVS_Linkage* const vectors)
 {
+    AVS_linkage = vectors;
     env->AddFunction("MPEG2Source", "[d2v]s[cpu]i[idct]i[iPP]b[moderate_h]i[moderate_v]i[showQ]b[fastMC]b[cpu2]s[info]i[upConv]i[i420]b[iCC]b", Create_MPEG2Source, 0);
     env->AddFunction("LumaYV12","c[lumoff]i[lumgain]f",Create_LumaYV12,0);
     env->AddFunction("BlindPP", "c[quant]i[cpu]i[cpu2]s[iPP]b[moderate_h]i[moderate_v]i", Create_BlindPP, 0);

build/msvc8/DGDecode/DGDecode.vcproj
@@ -237,7 +237,7 @@
 			UniqueIdentifier="{93995380-89BD-4b04-88EB-625FBE52EBFB}"
 			>
 			<File
-				RelativePath="..\..\..\src\dgdecode\avisynth2.h"
+				RelativePath="..\..\..\src\dgdecode\avisynth.h"
 				>
 			</File>
 			<File

build/msvc9/DGDecode/DGDecode.vcproj
@@ -233,7 +233,7 @@
 			UniqueIdentifier="{93995380-89BD-4b04-88EB-625FBE52EBFB}"
 			>
 			<File
-				RelativePath="..\..\..\src\dgdecode\avisynth2.h"
+				RelativePath="..\..\..\src\dgdecode\avisynth.h"
 				>
 			</File>
 			<File

build/msvcX/DGDecode/DGDecode.vcxproj
@@ -105,7 +105,7 @@
     <ClCompile Include="..\..\..\src\dgdecode\PostProcess.cpp" />
   </ItemGroup>
   <ItemGroup>
-    <ClInclude Include="..\..\..\src\dgdecode\avisynth2.h" />
+    <ClInclude Include="..\..\..\src\dgdecode\avisynth.h" />
     <ClInclude Include="..\..\..\src\dgdecode\global.h" />
     <ClInclude Include="..\..\..\src\dgdecode\resource.h" />
     <ClInclude Include="..\..\..\src\dgdecode\skl_nasm.h" />

build/msvcX/DGDecode/DGDecode.vcxproj.filters
@@ -80,7 +80,7 @@
     </ClCompile>
   </ItemGroup>
   <ItemGroup>
-    <ClInclude Include="..\..\..\src\dgdecode\avisynth2.h">
+    <ClInclude Include="..\..\..\src\dgdecode\avisynth.h">
       <Filter>Header Files</Filter>
     </ClInclude>
     <ClInclude Include="..\..\..\src\dgdecode\global.h">

src/dgdecode/AvisynthAPI.h
@@ -24,7 +24,7 @@
  */
 
 #include "global.h"
-#include "avisynth2.h"
+#include "avisynth.h"
 #include "text-overlay.h"
 
 #define uc unsigned char
@@ -50,7 +50,7 @@ public:
   bool __stdcall GetParity(int n);
   void __stdcall GetAudio(void* buf, __int64 start, __int64 count, IScriptEnvironment* env) {};
   const VideoInfo& __stdcall GetVideoInfo() { return vi; }
-  void __stdcall SetCacheHints(int cachehints,int frame_range) { } ;  // We do not pass cache requests upwards, only to the next filter.
+  int __stdcall SetCacheHints(int cachehints,int frame_range) { return 0; } ;  // We do not pass cache requests upwards, only to the next filter.
   void override(int ovr_idct);
   void conv422toYUV422(const unsigned char *py, unsigned char *pu, unsigned char *pv, unsigned char *dst,
                        int pitch1Y, int pitch1UV, int pitch2, int width, int height);

commit e1d31f2ad96eb57922c8fffe6c7b7745a0304465

DGDecode: Remove the dependency on the helper function of avisynth from non-avisynth function
Regression in 6e7200d.
DGVFapi was broken.

src/dgdecode/AVISynthAPI.cpp
@@ -1860,6 +1860,16 @@ public:
 NoAVSAccess g_A;
 NoAVSLinkedList g_LL;
 
+// Import from avisynth v2.61 [avisynth/src/core/interface.cpp]
+static inline bool IsYV12( VideoInfo* vi )
+{
+    return (vi->pixel_type & VideoInfo::CS_PLANAR_MASK) == (VideoInfo::CS_YV12 & VideoInfo::CS_PLANAR_FILTER);
+}
+static inline void SetFieldBased( VideoInfo* vi, bool isfieldbased )
+{
+    if (isfieldbased) vi->image_type |= VideoInfo::IT_FIELDBASED; else vi->image_type &= ~VideoInfo::IT_FIELDBASED;
+}
+
 MPEG2Source::MPEG2Source(const char* d2v, int _upConv)
 {
     CheckCPU();
@@ -1912,7 +1922,7 @@ MPEG2Source::MPEG2Source(const char* d2v, int _upConv)
     vi.fps_numerator = m_decoder.VF_FrameRate_Num;
     vi.fps_denominator = m_decoder.VF_FrameRate_Den;
     vi.num_frames = m_decoder.VF_FrameLimit;
-    vi.SetFieldBased(false);
+    SetFieldBased(&vi, false);
 
     bufY = bufU = bufV = NULL;
     u444 = v444 = NULL;
@@ -1924,14 +1934,14 @@ void MPEG2Source::GetFrame(int n, unsigned char *buffer, int pitch)
 {
     out->y = buffer;
     out->u = out->y + (pitch * m_decoder.Coded_Picture_Height);
-    if (vi.IsYV12()) out->v = out->u + ((pitch * m_decoder.Chroma_Height)/2);
+    if (IsYV12(&vi)) out->v = out->u + ((pitch * m_decoder.Chroma_Height)/2);
     else out->v = out->u + ((pitch * m_decoder.Coded_Picture_Height)/2);
     out->ypitch = pitch;
     out->uvpitch = pitch/2;
     out->ywidth = m_decoder.Coded_Picture_Width;
     out->yheight = m_decoder.Coded_Picture_Height;
     out->uvwidth = m_decoder.Chroma_Width;
-    if (vi.IsYV12()) out->uvheight = m_decoder.Chroma_Height;
+    if (IsYV12(&vi)) out->uvheight = m_decoder.Chroma_Height;
     else out->uvheight = m_decoder.Coded_Picture_Height;
 
     m_decoder.Decode(n, out);