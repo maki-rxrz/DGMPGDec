1403e9182f0562af789cd3b28c4f3e03ccb8c44d
Masaki Tanaka <maki.rxrz@gmail.com>
2017-03-19 00:00:00 +0900

    all: Add build batches for MSVC 2017

---
+++ nmake_build_msvc15.bat
@@ -0,0 +1,31 @@
+@echo off
+
+setlocal
+
+set VS_NUMBER=2017
+set VS_EDITION=Community
+
+set VSINSTPATH=%ProgramFiles(x86)%\Microsoft Visual Studio\%VS_NUMBER%\%VS_EDITION%
+
+rem Set up NASMPATH (Path termination must be a separator character.)
+rem set NASMPATH=<YouNasmPath>\
+
+set EXTRA_CPPFLAGS=/FS
+
+cd .\src\dgindex
+
+call "%VSINSTPATH%\VC\Auxiliary\Build\vcvarsall.bat" x86
+
+jom /version >NUL 2>&1
+
+if "%ERRORLEVEL%" == "0" (
+	jom
+) else (
+	nmake /nologo
+)
+
+echo.
+
+endlocal
+
+pause
