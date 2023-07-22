3e3b8a3a72c13a420d1b1553b2eae34c4181a7c8
Masaki Tanaka <maki.rxrz@gmail.com>
2023-07-23 00:00:00 +0900

    all: Add build batches for MSVC 2022

---
+++ DGMPGDec_Build_Release_msvc17.bat
@@ -0,0 +1,58 @@
+@echo off
+
+setlocal
+
+echo -----------------------------------------------------
+echo                   S T A R T
+echo -----------------------------------------------------
+
+set VisualStudioVersion=17.0
+set TOOLS_VER=Current
+set MSVC_VER=msvc17
+set VS_NUMBER=2022
+set VS_EDITION=Community
+
+set VSINSTPATH=%ProgramFiles(x86)%\Microsoft Visual Studio\%VS_NUMBER%\%VS_EDITION%
+
+rem Check installed version of Visual Studio (15.2 or later)
+set VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+if exist "%VSWHERE%" (
+    for /f "delims=. tokens=1" %%a in ('"%VSWHERE%" -products * -property installationVersion') do set VisualStudioVersion=%%a.0
+    for /f "delims=. tokens=1" %%a in ('"%VSWHERE%" -products * -property installationVersion') do set MSVC_VER=msvc%%a
+    for /f "delims="           %%a in ('"%VSWHERE%" -products * -property installationPath'   ) do set VSINSTPATH=%%a
+)
+
+rem Set up for MSBuild
+rem [memo] 4.0: %SystemRoot%\Microsoft.NET\Framework\v4.0.30319
+rem  12.0-14.0: %ProgramFiles(x86)%\MSBuild\%TOOLS_VER%\Bin
+set MSBUILD_PATH=%VSINSTPATH%\MSBuild\%TOOLS_VER%\Bin
+set MSBUILD_EXEC=%MSBUILD_PATH%\MSBuild.exe
+
+rem [memo] verbosity-level: q[uiet] m[inimal] n[ormal] d[etailed] diag[nostic]
+set BUILD_OPT=/nologo /maxcpucount:1 /toolsversion:%TOOLS_VER% /verbosity:normal /fl
+set FLOG_VERBOSITY=normal
+
+rem List up the target project
+set TGT_PROJECT=DGMPGDec
+set TGT_SLN=.\msvc\%TGT_PROJECT%.sln
+set TGT_PLATFORM=Win32
+set TGT_CONFIG=Release
+
+rem Set up NASMPATH (Path termination must be a separator character.)
+rem set NASMPATH=<YourNasmPath>\
+
+rem Windows XP support
+rem set WINXPISDEAD=1
+
+rem Build
+for %%c in (%TGT_CONFIG%  ) do (
+for %%p in (%TGT_PLATFORM%) do (
+    echo -Build-  %%c^|%%p
+    "%MSBUILD_EXEC%" %TGT_SLN% /p:Configuration=%%c;Platform=%%p %BUILD_OPT% /flp:logfile=_build_%TGT_PROJECT%_%%c_%%p_%MSVC_VER%.log;verbosity=%FLOG_VERBOSITY%
+))
+
+echo -----------------------------------------------------
+echo                   F I N I S H
+echo -----------------------------------------------------
+
+endlocal
---
+++ nmake_build_msvc17.bat
@@ -0,0 +1,40 @@
+@echo off
+
+setlocal
+
+set VS_NUMBER=2022
+set VS_EDITION=Community
+
+set VSINSTPATH=%ProgramFiles(x86)%\Microsoft Visual Studio\%VS_NUMBER%\%VS_EDITION%
+
+rem Check installed version of Visual Studio (15.2 or later)
+set VSWHERE=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+if exist "%VSWHERE%" (
+    for /f "delims=" %%a in ('"%VSWHERE%" -products * -property installationPath') do set VSINSTPATH=%%a
+)
+
+rem Set up NASMPATH (Path termination must be a separator character.)
+rem set NASMPATH=<YouNasmPath>\
+
+rem Windows XP support
+rem set WINXPISDEAD=1
+
+set EXTRA_CPPFLAGS=/FS
+
+cd .\src\dgindex
+
+call "%VSINSTPATH%\VC\Auxiliary\Build\vcvarsall.bat" x86
+
+jom /version >NUL 2>&1
+
+if "%ERRORLEVEL%" == "0" (
+	jom
+) else (
+	nmake /nologo
+)
+
+echo.
+
+endlocal
+
+pause
